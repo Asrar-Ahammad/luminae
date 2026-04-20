f30cecd5735eb4ebc7a36c2bc2d7003bbb2d8f7f
Asrar Ahammad <asrarahammadshaik@gmail.com>
2026-04-20 23:43:05 +0530

    feat: refactor sign-in page with Suspense, add Supabase dependencies, and update project configuration

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, env } from "prisma/config";
 
 
 export default defineConfig({
-  earlyAccess: true,
   schema: "prisma/schema.prisma",
   datasource: {
     url: env("DATABASE_URL"),
